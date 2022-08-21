7670797504fa855792b32514722bc165c87c4a5b
Ritu Singh <69470113+RituSingh2002@users.noreply.github.com>
2022-08-21 13:38:51 +0530

    your message

---
+++ database.js
@@ -0,0 +1,23 @@
+ 
+const mongoose = require("mongoose");
+ 
+ 
+
+class Database {
+
+    constructor() {
+        this.connect();
+    }
+
+    connect() {
+        mongoose.connect("mongodb+srv://Ritu:123@cluster0.qi2hwt0.mongodb.net/TwitterCloneDB?retryWrites=true&w=majority")
+        .then(() => {
+            console.log("database connection successful");
+        })
+        .catch((err) => {
+            console.log("database connection error " + err);
+        })
+    }
+}
+
+module.exports = new Database();
